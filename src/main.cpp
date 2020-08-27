4800e6aded59e7c939817fc882e2f8dec01285c5
Schlegel Flegel <amonkbenson@gmail.com>
2020-08-27 13:24:35 +0200

    removed ble midi and added some color tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,55 @@
 #include <Arduino.h>
-
-#include <BLEMidi.h>
 #include <FastLED.h>
 
 
-#define MIDI_DEVICE_NAME "Dubstep Zauberfloete"
+#define TICKS_PER_SECOND 120
 
-#define LEDS_PIN 15
-#define LEDS_LENGTH 14
+#define ANIMATION_SINELON_NUM_COLORS 3
+
+#define PIN_LEDS 15
+#define NUM_LEDS 11
 
 
-BLEMIDI_CREATE_INSTANCE(midi);
-CRGB leds[LEDS_LENGTH];
+CRGB leds[NUM_LEDS];
+void (*animation)() = NULL;
+
+CRGB animation_sinelon_colors[] = { CRGB(0, 16, 32), CRGB(32, 0, 16), CRGB(24, 24, 24) };
+
+
+void animation_sinelon() {
+    int i, pos, phase;
+    
+    // Gradually fade to black
+    fadeToBlackBy(leds, NUM_LEDS, 20);
+
+    // Set a different color ad multiple points
+    for (i = 0; i < ANIMATION_SINELON_NUM_COLORS; i++) {
+        phase = UINT16_MAX / ANIMATION_SINELON_NUM_COLORS * i;
+        pos = beatsin16(20, 0, NUM_LEDS - 1, 0, phase);
+
+        // Add the color for a smoother animation
+        leds[pos] += animation_sinelon_colors[i];
+    }
+}
 
 
 void setup() {
     Serial.begin(115200);
 
-    // Setup the BLE Midi connection
-    midi.begin(MIDI_DEVICE_NAME);
-    FastLED.addLeds<NEOPIXEL, LEDS_PIN>(leds, LEDS_LENGTH);
+    // Setup the led strip
+    FastLED.addLeds<NEOPIXEL, PIN_LEDS>(leds, NUM_LEDS);
+    animation = NULL;
 }
 
 void loop() {
-    Serial.println("Sending Note");
+    // Play any active animation
+    if (animation) animation();
 
-    // Note On
-    midi.sendNoteOn(60, 127, 1);
-    leds[0] = CRGB::Blue;
-    FastLED.show();
-    delay(500);
+    // IR sensor test
+    int value = 255 - analogRead(32) * 255 / 4095;
+    fill_solid(leds, NUM_LEDS, CHSV(200, 200, value));
 
-    // Note Off
-    midi.sendNoteOn(60, 0, 1);
-    leds[0] = CRGB::Black;
+    // Update and delay
     FastLED.show();
-    delay(500);
+    FastLED.delay(1000 / TICKS_PER_SECOND);
 }
