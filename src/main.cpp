037a7d6e1d9432b48d3ac88111d27a7e770bdab9
Schlegel Flegel <amonkbenson@gmail.com>
2020-08-28 21:25:08 +0200

    First version of the final program

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,172 @@
 #include <Arduino.h>
+#include <Bounce2.h>
 #include <FastLED.h>
+#include <Midi.h>
+
+#include "palette.h"
 
 
-#define TICKS_PER_SECOND 120
+// Led strip
+#define LEDS_COUNT 11
+#define LEDS_PIN 15
 
-#define ANIMATION_SINELON_NUM_COLORS 3
+// Air sensor
+#define AIR_PIN 34
 
-#define PIN_LEDS 15
-#define NUM_LEDS 11
+#define AIR_DEBOUNCE_INTERVAL 50
+
+// Holes
+#define HOLES_COUNT 5
+#define HOLES_PINS { 33, 25, 12, 27, 14 }
+#define HOLES_AMPLIFICATION { 5.0, 8.0, 20.0, 5.0, 5.0 }
+#define HOLES_OFFSET { 0.1, 0.1, 0.1, 0.1, 0.1 }
+#define HOLES_ACTIVE_THRESH 0.9
+
+#define HOLES_CALIBRATION_DELAY 30
+#define HOLES_CALIBRATION_ITERATIONS 5
+
+// Midi
+#define MIDI_ROOTNOTE 36 // C2
+
+// General settings
+#define ANALOG_READ_MAX 4095
 
 
-CRGB leds[NUM_LEDS];
-void (*animation)() = NULL;
 
-CRGB animation_sinelon_colors[] = { CRGB(0, 16, 32), CRGB(32, 0, 16), CRGB(24, 24, 24) };
+CRGB leds[LEDS_COUNT];
 
 
-void animation_sinelon() {
-    int i, pos, phase;
-    
-    // Gradually fade to black
-    fadeToBlackBy(leds, NUM_LEDS, 20);
+Bounce air = Bounce();
+bool airFlowing = false;
 
-    // Set a different color ad multiple points
-    for (i = 0; i < ANIMATION_SINELON_NUM_COLORS; i++) {
-        phase = UINT16_MAX / ANIMATION_SINELON_NUM_COLORS * i;
-        pos = beatsin16(20, 0, NUM_LEDS - 1, 0, phase);
 
-        // Add the color for a smoother animation
-        leds[pos] += animation_sinelon_colors[i];
+const int holesPins[] = HOLES_PINS;
+const float holesAmplification[] = HOLES_AMPLIFICATION;
+const float holesOffset[] = HOLES_OFFSET;
+
+int holesThresh[HOLES_COUNT];
+bool holes[HOLES_COUNT];
+
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+midi::DataByte currentNote = 0;
+
+
+
+void calibrateHoles() {
+    float value;
+
+    for (int i = 0; i < HOLES_COUNT; i++) {
+        // Take a few readings
+        value = 0;
+        for (int j = 0; j < HOLES_CALIBRATION_ITERATIONS; j++) {
+            value += analogRead(holesPins[i]) / (float) HOLES_CALIBRATION_ITERATIONS;
+            delay(HOLES_CALIBRATION_DELAY);
+        }
+
+        // Set the threshold
+        holesThresh[i] = (int) value;
     }
+}
+
+void updateHoles() {
+    // Update the holes
+    float value;
+    bool hole, recalculateNote;
+    for (int i = 0; i < HOLES_COUNT; i++) {
+        // Read the value
+        value = (holesThresh[i] - analogRead(holesPins[i])) / (float) ANALOG_READ_MAX * holesAmplification[i] - holesOffset[i];
+        if (value < 0) value = 0;
+        if (value > 1) value = 1;
+
+        // Set the hole value
+        hole = value > HOLES_ACTIVE_THRESH;
+        holes[i] = hole;
+
+        // TODO: Check if the note changes while playing
+    }
+}
+
+void updateCurrentNote() {
+    //  Binary encode the first 3 holes to get the octave and the lower 2 for the note
+    midi::DataByte octave = holes[0] | holes[1] << 1 | holes[2] << 2;
+    midi::DataByte note = holes[3] | holes[4] << 1;
+
+    // Update the current note
+    currentNote = MIDI_ROOTNOTE + octave * 12 + note;
+}
+
+
+void handleNoteOn(byte channel, byte note, byte velocity) {
+    // Get the led index
+    int led = note - MIDI_ROOTNOTE;
+    if (led < 0) led = 0;
+    if (led >= LEDS_COUNT) led = LEDS_COUNT - 1;
+
+    // Get the color from the palette
+    leds[led] = colorPalette[velocity];
+}
+
+void handleNoteOff(byte channel, byte note, byte velocity) {
+    // Turn the led off (velocity 0)
+    handleNoteOn(channel, note, 0);
 }
 
 
 void setup() {
+    // Turn off the buildin led
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // Start midi with custom baud rate
+    MIDI.setHandleNoteOn(handleNoteOn);
+    MIDI.setHandleNoteOff(handleNoteOff);
+    MIDI.begin(MIDI_CHANNEL_OMNI);
     Serial.begin(115200);
+    currentNote = 0;
 
     // Setup the led strip
-    FastLED.addLeds<NEOPIXEL, PIN_LEDS>(leds, NUM_LEDS);
-    animation = NULL;
+    FastLED.addLeds<NEOPIXEL, LEDS_PIN>(leds, LEDS_COUNT);
+
+    // Setup the air sensor
+    air.attach(AIR_PIN, INPUT);
+    air.interval(0);
+
+    // Setup the holes
+    for (int i = 0; i < HOLES_COUNT; i++) {
+        pinMode(holesPins[i], INPUT);
+    }
+
+    // Start calibrating
+    calibrateHoles();
 }
 
 void loop() {
-    // Play any active animation
-    if (animation) animation();
+    // Check which holes are covered
+    updateHoles();
+    
+    // Check if air is flowing
+    air.update();
+    if (air.fell()) {
+        // Once air is flowing, activate the debouncing
+        air.interval(AIR_DEBOUNCE_INTERVAL);
+        airFlowing = true;
 
-    // IR sensor test
-    int value = 255 - analogRead(32) * 255 / 4095;
-    fill_solid(leds, NUM_LEDS, CHSV(200, 200, value));
+        // Start playing
+        updateCurrentNote();
+        MIDI.sendNoteOn(currentNote, 127, 1);
+    } else if (air.rose()) {
+        // If the air flow is done, deactivate debouncing by setting the interval to zero
+        air.interval(0);
+        airFlowing = false;
 
-    // Update and delay
+        // Stop playing
+        MIDI.sendNoteOn(currentNote, 0, 1);
+    }
+    
+    // Read any incoming midi messages
+    MIDI.read();
+
+    // Update
     FastLED.show();
-    FastLED.delay(1000 / TICKS_PER_SECOND);
 }
